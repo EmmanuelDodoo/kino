2d3f6956d1745a7daffc9ef7000f5a5b0faf3dba
emmanuel dodoo <custosdodoo@gmail.com>
2026-07-14 14:28:53 -0400

    feat: Added serde impls to Theme

--- src/theme.rs
+++ src/theme.rs
@@ -1,4 +1,5 @@
 use iced::{color, color::Color, theme};
+use serde::{Deserialize, Serialize, de, ser};
 
 use std::{
     borrow::Cow,
@@ -424,13 +425,47 @@ mod variants {
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq)]
+fn se_color<S: ser::Serializer>(color: &Color, s: S) -> Result<S::Ok, S::Error> {
+    use ser::Serializer;
+    let color = color.to_string();
+
+    s.serialize_str(&color)
+}
+
+fn de_color<'de, D: de::Deserializer<'de>>(d: D) -> Result<Color, D::Error> {
+    use de::{Deserializer, Visitor};
+
+    struct ColorVisitor;
+
+    impl<'de> Visitor<'de> for ColorVisitor {
+        type Value = Color;
+
+        fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+            write!(formatter, "a valid Color string")
+        }
+
+        fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+        where
+            E: de::Error,
+        {
+            use de::Error;
+
+            v.parse::<Color>().map_err(|error| de::Error::custom(error))
+        }
+    }
+
+    d.deserialize_str(ColorVisitor)
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
 pub struct Pair {
+    #[serde(serialize_with = "se_color", deserialize_with = "de_color")]
     pub color: Color,
+    #[serde(serialize_with = "se_color", deserialize_with = "de_color")]
     pub text: Color,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq)]
+#[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
 pub struct Triplet {
     pub weak: Pair,
     pub base: Pair,
@@ -455,7 +490,7 @@ impl Triplet {
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq)]
+#[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
 pub struct Schema {
     pub background: Triplet,
     pub primary: Triplet,
@@ -470,17 +505,17 @@ pub struct Schema {
     pub radii: Radii,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq)]
+#[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
 pub struct Radii {
     /// Cards, modals, alerts, Containers
-    boxes: f32,
+    pub boxes: f32,
     /// Buttons, inputs, tabs
-    fields: f32,
+    pub fields: f32,
     /// Checkbox, toggle, badge
-    selectors: f32,
+    pub selectors: f32,
 }
 
-#[derive(Debug, Clone, PartialEq, Default)]
+#[derive(Debug, Clone, PartialEq, Default, Serialize, Deserialize)]
 pub enum Theme {
     #[default]
     Autumn,
@@ -490,6 +525,7 @@ pub enum Theme {
     Fantasy,
     Black,
     Abyss,
+    #[serde(serialize_with = "se_custom", deserialize_with = "de_custom")]
     Custom(Arc<Custom>),
 }
 
@@ -584,7 +620,7 @@ impl std::fmt::Display for Theme {
     }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Custom {
     pub name: Cow<'static, str>,
     pub schema: Schema,
@@ -594,4 +630,16 @@ impl From<Custom> for Theme {
     fn from(value: Custom) -> Self {
         Theme::Custom(Arc::new(value))
     }
+}
+
+fn se_custom<S: ser::Serializer>(field: &Arc<Custom>, s: S) -> Result<S::Ok, S::Error> {
+    let custom = field.as_ref();
+
+    custom.serialize(s)
+}
+
+fn de_custom<'de, D: de::Deserializer<'de>>(d: D) -> Result<Arc<Custom>, D::Error> {
+    let custom = Arc::new(Custom::deserialize(d)?);
+
+    Ok(custom)
 }
